22310e9ed7f5575a73918de601b8c9fdd40cffaf
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-11-25 16:46:18 +0300

    begin to learn cv

--- lesson_4.py
+++ lesson_4.py
@@ -1,25 +1,25 @@
 '''
 https://www.youtube.com/watch?v=cobu9gdC0Ao
-videos
+rotate, mirror, transform, read and draw
 '''
 import cv2
 import numpy as np
 
-# cap = cv2.VideoCapture('videos/Mountain.mp4')
-#
-# while True:
-#     success, img = cap.read()
-#
-#     img = cv2.GaussianBlur(img, (9, 9), 0)
-#     img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
-#     img = cv2.Canny(img, 30, 30)
-#
-#     kernel = np.ones((5, 5), np.uint8)
-#     img = cv2.dilate(img, kernel, iterations=1)
-#
-#     img = cv2.erode(img, kernel, iterations=1)
-#
-#     cv2.imshow('videos', img)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+def rotate(img_param, angle):
+    height, width = img_param.shape[:2]
+    point = (width // 2, height // 2)
+
+    mat = cv2.getRotationMatrix2D(point, angle, 0.5)
+    return cv2.warpAffine(img_param, mat, (width, height))
+
+def transform(img_param, x, y):
+    mat = np.float32([[1, 0, x], [0, 1, y]])
+    return cv2.warpAffine(img_param, mat, (img_param.shape[1], img_param.shape[0]))
+
+img = cv2.imread('images/kids.jpg')
+# img = cv2.flip(img, 1)
+# img = rotate(img, -90)
+img = transform(img, 20, 80)
+cv2.imshow('kids', img)
+
+cv2.waitKey(0)
