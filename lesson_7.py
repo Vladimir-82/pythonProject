c1ff231ec050bd6a96d956ab60efc396cd52ffda
vladimir <grun_gespenst@tut.by>
2021-11-27 13:21:24 +0300

    rect_faces_started

--- lesson_7.py
+++ lesson_7.py
@@ -1,3 +1,17 @@
 '''
 https://www.youtube.com/watch?v=1biHdzEkAJg
 '''
+import cv2
+
+img = cv2.imread('images/image2.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = cv2.CascadeClassifier('videos/faces.xml')
+
+result = faces.detectMultiScale(img, scaleFactor=1, minNeighbors=3)
+
+for (x, y, w, h) in result:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=3)
+
+cv2.imshow('res', img)
+cv2.waitKey(0)
