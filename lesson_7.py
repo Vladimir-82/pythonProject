1cfb7c558bc0dd7d0093c4eeb119545d5029c632
vladimir <grun_gespenst@tut.by>
2021-11-28 09:28:55 +0300

    cv

--- lesson_7.py
+++ lesson_7.py
@@ -3,12 +3,12 @@ https://www.youtube.com/watch?v=1biHdzEkAJg
 '''
 import cv2
 
-img = cv2.imread('images/image2.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('images/image1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = cv2.CascadeClassifier('videos/faces.xml')
 
-result = faces.detectMultiScale(img, scaleFactor=1, minNeighbors=3)
+result = faces.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
 for (x, y, w, h) in result:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=3)
